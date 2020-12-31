7f155cf343e81edbffcc7d7daef95aa681f4b7cf
dpak1999 <deepakkumardash313@gmail.com>
2020-12-31 22:17:21 +0530

    remove transition after click

--- day-1(drumkit)/script.js
+++ day-1(drumkit)/script.js
@@ -1,4 +1,4 @@
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;
@@ -7,4 +7,14 @@ window.addEventListener("keydown", function (e) {
   audio.play();
 
   key.classList.add("playing");
-});
+}
+
+window.addEventListener("keydown", playSound);
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
